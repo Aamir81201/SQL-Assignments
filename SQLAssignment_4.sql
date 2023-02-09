0a567ebcddcc9f0240ce024b59bab557e66953df
Mohammad Dalal <PCE49@server7.com>
2023-02-09 10:58:35 +0530

    Assignment-4 Complete

--- SQLAssignment_4.sql
+++ SQLAssignment_4.sql
@@ -3,14 +3,14 @@ GO
 
 ------------------------(1)------------------------------------------------------------------------------------------------------------------------------
 
-ALTER PROCEDURE spAvgFreight
+CREATE PROCEDURE spAvgFreight
 @CustomerID nchar(5),
 @AverageFreight REAL OUTPUT
 AS
 BEGIN
 	SELECT @AverageFreight = AVG(Freight) from Orders WHERE CustomerID = @CustomerID
 END
-
+GO
 
 CREATE TRIGGER tr_orders_insert
 ON Orders
@@ -43,24 +43,145 @@ BEGIN
 END
 GO
 
---Valid (Avg = 23)
+
+CREATE TRIGGER tr_orders_update
+ON Orders
+INSTEAD OF UPDATE
+AS
+BEGIN
+
+	IF(UPDATE(Freight))
+	BEGIN
+		DECLARE @cust nchar(5);
+		DECLARE @freight REAL;
+		DECLARE @avg REAL;
+
+		SELECT @Cust = I.CustomerID FROM Orders O INNER JOIN inserted I ON O.CustomerID = I.CustomerID
+		SELECT @freight = I.Freight FROM Orders O INNER JOIN inserted I ON O.CustomerID = I.CustomerID
+
+		EXEC spAvgFreight
+			@CustomerID = @Cust,
+			@AverageFreight = @avg OUTPUT;
+	
+			IF(@freight<@avg)
+			BEGIN
+				Print('Valid Freight : Updated Freight Successfully')
+				
+				UPDATE Orders
+				SET Freight = @freight
+				FROM inserted I
+				INNER JOIN Orders O
+				ON I.OrderID = O.OrderID
+			END
+
+			ELSE
+			BEGIN
+				Print('Failed : Update Freight has exceeded average Freight value of given Customer')
+			END
+	END
+
+	IF(UPDATE(OrderID))
+	BEGIN
+		Raiserror('Cannot update OrderID',16,1)
+		return
+	END
+	IF(UPDATE(CustomerID))
+	BEGIN
+		Raiserror('Cannot update CustomerID',16,1)
+		return
+	END
+	IF(UPDATE(EmployeeID))
+	BEGIN
+		Raiserror('Cannot update EmployeeID',16,1)
+		return
+	END
+	IF(UPDATE(OrderDate))
+	BEGIN
+		Raiserror('Cannot update OrderDate',16,1)
+		return
+	END
+	IF(UPDATE(RequiredDate))
+	BEGIN
+		Raiserror('Cannot update RequiredDate',16,1)
+		return
+	END
+	IF(UPDATE(ShippedDate))
+	BEGIN
+		Raiserror('Cannot update ShippedDate',16,1)
+		return
+	END
+	IF(UPDATE(ShipVia))
+	BEGIN
+		Raiserror('Cannot update ShipVia',16,1)
+		return
+	END
+	IF(UPDATE(ShipName))
+	BEGIN
+		Raiserror('Cannot update ShipName',16,1)
+		return
+	END
+	IF(UPDATE(ShipAddress))
+	BEGIN
+		Raiserror('Cannot update ShipAddress',16,1)
+		return
+	END
+	IF(UPDATE(ShipCity))
+	BEGIN
+		Raiserror('Cannot update ShipCity',16,1)
+		return
+	END
+	IF(UPDATE(ShipRegion))
+	BEGIN
+		Raiserror('Cannot update ShipRegion',16,1)
+		return
+	END
+	IF(UPDATE(ShipPostalCode))
+	BEGIN
+		Raiserror('Cannot update ShipPostalCode',16,1)
+		return
+	END
+	IF(UPDATE(ShipCountry))
+	BEGIN
+		Raiserror('Cannot update ShipCountry',16,1)
+		return
+	END
+END
+GO
+
+--------Valid---------------------------------------------------------
+
+--insert
 INSERT INTO "Orders"
-("CustomerID","EmployeeID","OrderDate","RequiredDate",
-	"ShippedDate","ShipVia","Freight","ShipName","ShipAddress",
-	"ShipCity","ShipRegion","ShipPostalCode","ShipCountry")
+(CustomerID,EmployeeID,OrderDate,RequiredDate,ShippedDate,
+	ShipVia,Freight,ShipName,ShipAddress,ShipCity,ShipRegion,
+	ShipPostalCode,ShipCountry)
 VALUES (N'REGGC',4,'8/23/1996','9/20/1996','9/3/1996',1,16.45,
 	N'Reggiani Caseifici',N'Strada Provinciale 124',N'Reggio Emilia',
 	NULL,N'42100',N'Italy')
 GO
 
---Invalid (Avg = 23)
-INSERT INTO "Orders"
-("CustomerID","EmployeeID","OrderDate","RequiredDate",
-	"ShippedDate","ShipVia","Freight","ShipName","ShipAddress",
-	"ShipCity","ShipRegion","ShipPostalCode","ShipCountry")
+--update
+UPDATE Orders
+SET Freight = 5
+WHERE OrderID = 10248
+GO
+
+--------Invalid--------------------------------------------------------
+
+--insert
+INSERT INTO Orders
+(CustomerID,EmployeeID,OrderDate,RequiredDate,ShippedDate,
+	ShipVia,Freight,ShipName,ShipAddress,ShipCity,
+	ShipRegion,ShipPostalCode,ShipCountry)
 VALUES (N'REGGC',4,'8/23/1996','9/20/1996','9/3/1996',1,60.45,
 	N'Reggiani Caseifici',N'Strada Provinciale 124',N'Reggio Emilia',
 	NULL,N'42100',N'Italy')
+GO
+
+--update
+UPDATE Orders
+SET Freight = 50
+WHERE OrderID = 10248
 GO
 
 
@@ -77,9 +198,10 @@ BEGIN
 	WHERE ShipCountry = ISNULL(@country,ShipCountry)
 	Group By E.EmployeeID, E.FirstName, E.LastName
 END
+GO
 
-spGetSalesByCountry 'UK'
-
+spGetSalesByCountry 'UK';
+GO
 
 ------------------------(3)------------------------------------------------------------------------------------------------------------------------------
 
@@ -93,8 +215,10 @@ BEGIN
 	ON OS.OrderID = O.OrderID
 	WHERE YEAR(ShippedDate) = @year
 END
+GO
 
 spGetSalesByYear '1996'
+GO
 
 
 ------------------------(4)------------------------------------------------------------------------------------------------------------------------------
@@ -111,8 +235,10 @@ spGetSalesByYear '1996'
 	WHERE P.CategoryID = @Category
 	GROUP BY P.ProductID, P.ProductName, C.CategoryName
  END
+ GO
 
  spGetSalesByCategories 7
+ GO
 
 
 ------------------------(5)------------------------------------------------------------------------------------------------------------------------------
@@ -124,7 +250,7 @@ BEGIN
 	FROM Products
 	ORDER BY UnitPrice DESC;
 END
-
+GO
 
 
 ------------------------(6)------------------------------------------------------------------------------------------------------------------------------
@@ -137,9 +263,29 @@ CREATE PROCEDURE spInsertCustomerOrderDetails
 @Discount REAL
 AS
 BEGIN
-INSERT "Order Details" VALUES(@OrderID,@ProductID,@UnitPrice,@Quantity,@Discount)
+	INSERT INTO [Order Details] 
+	VALUES(@OrderID,@ProductID,@UnitPrice,@Quantity,@Discount)
 END
+GO
 
-
+spInsertCustomerOrderDetails 10248, 20, '10', 5, 0
+GO
 
 ------------------------(7)------------------------------------------------------------------------------------------------------------------------------
+
+CREATE PROCEDURE spUpdateCustomerOrderDetails
+@OrderID INT,
+@ProductID INT,
+@UnitPrice MONEY, 
+@Quantity INT,
+@Discount REAL
+AS
+BEGIN
+	UPDATE [Order Details] 
+	SET Quantity = @Quantity, Discount = @Discount, UnitPrice = @UnitPrice
+	WHERE OrderID = @OrderID AND ProductID = @ProductID
+END
+GO
+
+spUpdateCustomerOrderDetails 10248, 15, '12', 10, 0.15
+GO
